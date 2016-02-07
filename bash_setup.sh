fbc5518ca3989f42b6a25bf94e58722a44086cd5
gyenney <gregory.yenney@gmail.com>
2016-02-06 16:14:11 -0800

    initial commit for the Tools collection.

---
+++ bash_setup.sh
@@ -0,0 +1,37 @@
+basedir=`pwd`
+
+echo PWD:  $basedir
+
+export PATH toolsdir srcdir
+
+toolsdir=$basedir
+
+PATH=$toolsdir/nodejs-x86:$PATH
+PATH=$toolsdir/putty/Putty-beta-0.65:$PATH
+PATH=$toolsdir/PortableGit:$PATH
+PATH=$toolsdir/WinPython-32bit-2.7.10.3:$PATH
+PATH=$toolsdir/WinPython-32bit-2.7.10.3/Python-2.7.10:$PATH
+
+echo --
+echo Environment setup is complete 
+echo --
+srcdir=$basedir/../Dev
+
+cd $srcdir
+
+curr_dir=`pwd`
+
+echo Current directory is set to $curr_dir 
+echo --
+echo Commands you can use include:
+echo --
+echo "putty     to connect to other machines."
+echo "git-bash  to use git and do lots of other things."
+echo "node      to run node-JS programs.  E.g.  node your_prog.js"
+echo "idle      to run the python IDE."
+echo "python    to run your python programs E.g. python your_program.py"
+echo "notepad   to write your source-code."
+echo --
+echo Please save your programs on the flash drive in $curr_dir
+echo --
+echo You are now ready to program!
